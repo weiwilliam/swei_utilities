ec8c09864f4a121f8d54fe20479feee65c27059e
weiwilliam <weiwilliam1987@gmail.com>
2022-04-14 17:37:49 +0000

    hera python script

--- machine/hera/sub_python.sh
+++ machine/hera/sub_python.sh
@@ -1,11 +1,16 @@
 #!/bin/ksh -ax
 #SBATCH --job-name=runpython
-#SBATCH --time=00:30:00
+#SBATCH --time=06:00:00
 #SBATCH --account=gsd-fv3-dev
-#SBATCH --qos=debug
-#SBATCH --ntasks=1
+#SBATCH --partition=bigmem
+#SBATCH --qos=batch
+#SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=6000
+#SBATCH --exclusive
+##SBATCH --qos=debug
+##SBATCH --ntasks=1
+##SBATCH --cpus-per-task=1
+##SBATCH --mem-per-cpu=6000
 #SBATCH --output=/scratch1/BMC/gsd-fv3-dev/Shih-wei.Wei/wrklog/runpython.log.%j
 
 . /apps/lmod/lmod/init/sh
@@ -13,5 +18,7 @@ module load intel/18.0.5.274
 module load anaconda/latest
 
 export HDF5_USE_FILE_LOCKING='FALSE'
+#pyscript=/home/Shih-wei.Wei/research/pyscripts/ncrad_newQC_NormOMB.py
+#pyscript=/home/Shih-wei.Wei/research/pyscripts/ncrad_newQC_hist.py
 pyscript=/home/Shih-wei.Wei/research/pyscripts/ncrad_newQC_stats.py
 python3 $pyscript
