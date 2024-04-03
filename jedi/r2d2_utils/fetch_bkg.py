79dac60701a3c358e962de06cadcc9d4e8a4970f
weiwilliam <weiwilliam1987@gmail.com>
2024-04-03 19:53:56 +0000

    updates

--- jedi/r2d2_utils/fetch_bkg.py
+++ jedi/r2d2_utils/fetch_bkg.py
@@ -6,8 +6,8 @@ from r2d2 import R2D2Data
 
 member = R2D2Data.DEFAULT_INT_VALUE
 
-fetch = 1
-savedir = '/work2/noaa/jcsda/shihwei/data/bkg'
+fetch = 0
+savedir = '/data/users/swei/Dataset/jedi-data/bkg'
 date ='2021080506'
 fc_length = timedelta(hours=6)
 init_date = pd.to_datetime(date,format='%Y%m%d%H') - fc_length
