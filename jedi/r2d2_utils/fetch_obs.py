a0f97193eb1c19d7253db4e52c648c3b600a052d
weiwilliam <weiwilliam1987@gmail.com>
2024-07-08 19:53:07 +0000

    update r2d2 obs fetching

--- jedi/r2d2_utils/fetch_obs.py
+++ jedi/r2d2_utils/fetch_obs.py
@@ -3,46 +3,61 @@ import os,sys
 from r2d2 import R2D2Data
 import pandas as pd
 from datetime import timedelta
+from utils import get_dates
 
-fetch = 1
-savedir='/work2/noaa/jcsda/shihwei/save_geoval/input/obs'
-date='2021080506'
-half_win = timedelta(hours=6)/2
-step='PT6H'
-win_beg = (pd.to_datetime(date,format='%Y%m%d%H')-half_win).strftime('%Y-%m-%dT%H:00:00Z')
-obsname = 'viirs_n20_aod-thinned'
-provider = 'noaa'
+fetch = 0
+savedir = '/data/users/swei/Dataset/jedi-data/input/obs/tempo_no2_tropo-full'
+sdate = '2024062018'
+edate = '2024062100'
+hint = 6
+half_win = timedelta(hours=hint)/2
+step = 'PT6H'
+#win_beg = (pd.to_datetime(date,format='%Y%m%d%H')-half_win).strftime('%Y-%m-%dT%H:00:00Z')
+#obsname = 'tropomi_s5p_co_total'
+obsname = 'tempo_no2_tropo'
+provider = 'nasa'
 file_extension = 'nc4'
-obsfile = os.path.join(savedir,'obs.'+obsname+'.'+date+'.'+file_extension)
 
-conf = {
-        'obsname': obsname,
-        'window_begin': win_beg,
-        'window_length': step,
-        'obsfile': obsfile,
-        'file_extension':file_extension,
-        }
+print(f'Search {obsname} for cycle {sdate} to {edate} every {hint} hours from Provider: {provider}')
 
-result = R2D2Data.search(
-                        provider=provider,
-                        item='observation',
-                        observation_type=conf['obsname'],
-                        window_start=conf['window_begin'],
-                        window_length=conf['window_length'],
-                        )
 
-for item in result:
-    print(item)
+dates = get_dates(sdate, edate, 6)
+
+result = []
+total_conf = []
+for date in dates:
+    win_beg = (date - half_win).strftime('%Y-%m-%dT%H:00:00Z')
+    date_string = date.strftime('%Y%m%d%H')
+    obsfile = os.path.join(savedir,'obs.'+obsname+'.'+date_string+'.'+file_extension)
+    conf = {
+            'obsname': obsname,
+            'window_begin': win_beg,
+            'window_length': step,
+            'obsfile': obsfile,
+            'file_extension':file_extension,
+            }
+    
+    result += R2D2Data.search(
+                             provider=provider,
+                             item='observation',
+                             observation_type=conf['obsname'],
+                             window_start=conf['window_begin'],
+                             window_length=conf['window_length'],
+                             )
+    total_conf += [conf]
+
+for item, cyc_conf in zip(result, total_conf):
+    print(f"Fetch {item} to {cyc_conf['obsfile']}")
     if not fetch:
         continue
 
     R2D2Data.fetch(
-        provider=provider,
+        provider=item['provider'],
         item='observation',
-        observation_type=conf['obsname'],
-        window_start=conf['window_begin'],
-        window_length=conf['window_length'],
-        target_file=conf['obsfile'],
-        file_extension=conf['file_extension'],
+        observation_type=item['observation_type'],
+        window_start=item['window_start'],
+        window_length=item['window_length'],
+        target_file=cyc_conf['obsfile'],
+        file_extension=item['file_extension'],
     )
 
