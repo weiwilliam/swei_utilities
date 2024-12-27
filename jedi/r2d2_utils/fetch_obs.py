55a8beaf23cfc041dc017149f96b6c65d0d33c3f
weiwilliam <weiwilliam1987@gmail.com>
2024-12-27 12:41:50 -0600

    update r2d2 fetch_obs

--- jedi/r2d2_utils/fetch_obs.py
+++ jedi/r2d2_utils/fetch_obs.py
@@ -6,29 +6,28 @@ from datetime import timedelta
 from utils import get_dates
 
 fetch = 1
-savedir = '/data/users/swei/Dataset/jedi-data/input/obs/tempo_no2_tropo-full'
+savedir = '/work2/noaa/jcsda/shihwei/data/jedi-data/input/obs/tempo_no2_tropo-full'
 # savedir = '/data/users/swei/Dataset/jedi-data/input/obs/tropomi_s5p_no2_tropo-full'
 # savedir = '/glade/derecho/scratch/swei/Dataset/input/obs/ioda_tempo_no2_tropo-full'
-sdate = '2024060100'
-edate = '2024063018'
-hint = 6
+sdate = '2024082200'
+edate = '2024090100'
+hint = 1
 half_win = timedelta(hours=hint)/2
-step = 'PT6H'
+step = 'PT1H'
 #win_beg = (pd.to_datetime(date,format='%Y%m%d%H')-half_win).strftime('%Y-%m-%dT%H:00:00Z')
 #obsname = 'tropomi_s5p_co_total'
-obsname = 'tropomi_s5p_no2_tropo'
-provider = 'esa' # 'nasa'
+obsname = 'tempo_no2_tropo'
+provider = 'nasa' # 'nasa'
 file_extension = 'nc4'
 
 print(f'Search {obsname} for cycle {sdate} to {edate} every {hint} hours from Provider: {provider}')
 
-
-dates = get_dates(sdate, edate, 6)
+dates = get_dates(sdate, edate, hint)
 
 result = []
 total_conf = []
 for date in dates:
-    win_beg = (date - half_win).strftime('%Y-%m-%dT%H:00:00Z')
+    win_beg = (date - half_win).strftime('%Y-%m-%dT%H:%M:%SZ')
     date_string = date.strftime('%Y%m%d%H')
     obsfile = os.path.join(savedir,'obs.'+obsname+'.'+date_string+'.'+file_extension)
     conf = {
