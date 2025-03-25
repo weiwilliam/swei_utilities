0c2c493a3045bac46d5aeaf00914f5459cf005bd
weiwilliam <weiwilliam1987@gmail.com>
2025-03-25 12:03:26 -0500

    update iodaconv full validate script

--- jedi/ioda_utils/iodaconv_full_validate.sh
+++ jedi/ioda_utils/iodaconv_full_validate.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-IODA_BUNDLE=$1
-converter=$2
-converter_output=$3
-IODABUILD=$4
+converter=$1
+converter_output=$2
 
-pycodestyle -v --config="$IODA_BUNDLE/iodaconv/.pycodestyle" --filename=*.py,*.py.in "$IODA_BUNDLE/iodaconv/src/$converter"
+pycodestyle -v --config="$JEDI_SRC/iodaconv/.pycodestyle" --filename=*.py,*.py.in "$converter"
 
-$IODABUILD/bin/ioda-validate.x $IODA_BUNDLE/ioda/share/ioda/yaml/validation/ObsSpace.yaml $converter_output
+$JEDI_BUILD/bin/ioda-validate.x $JEDI_SRC/ioda/share/ioda/yaml/validation/ObsSpace.yaml $converter_output
