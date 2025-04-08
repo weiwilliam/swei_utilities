c51abe05044d3f12028ac759e2e5d9a1337f345c
weiwilliam <weiwilliam1987@gmail.com>
2025-04-08 15:00:05 -0600

    add NASA LAADS data pull script

---
+++ data_process/process_data_nasa_laads.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -x
+
+start_date=2020061812
+  end_date=2020061812
+window_len=6
+
+outpath='/glade/work/swei/data/RawOBS/VIIRS_L1b'
+year=2020
+jday=170
+datastream='5201'
+datatype='VJ102MOD'
+
+src_url="https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/${datastream}/${datatype}/${year}/${jday}"
+des_path=${outpath}/$datatype/$year/$jday
+
+[[ ! -d $des_path ]] && mkdir -p $des_path
+
+query_files='wget -q ${src_url} -O - | grep data-name | cut -d "=" -f3 | cut -d " " -f1 | rev | cut -c 2- | rev | cut -c 2- || echo ""'
+
+cycfiles=`eval $query_files`
+echo $cycfiles
+
+for file in $cycfiles
+do
+    wget -nc --auth-no-challenge --no-check-certificate ${src_url}/${file}
+done 
